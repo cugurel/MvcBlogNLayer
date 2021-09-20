2a3eb7068fbcc2558b73af974e1664f2641b8f8b
Çağrı Uğurel <cugurel7@gmail.com>
2021-09-21 01:12:50 +0300

    fluent

---
+++ BusinessLayer/ValidationRules/CategoryValidater.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidater:AbstractValidator<Category>
+    {
+        public CategoryValidater()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adı boş geçilemez");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama boş geçilemez");
+        }
+    }
+}
---
+++ WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categories = cm.GetCategoryList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category ct)
+        {
+            CategoryValidater cv = new CategoryValidater();
+            ValidationResult results = cv.Validate(ct);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(ct);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+            }
+            return View();
+        }
+    }
+}
