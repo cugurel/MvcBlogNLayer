c3e14114e77f1c63743b6535cfd5ede16b7b7087
Çağrı Uğurel <cugurel7@gmail.com>
2021-10-05 00:09:00 +0300

    modal

--- WebUI/Controllers/AdminAboutController.cs
+++ WebUI/Controllers/AdminAboutController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,24 @@ namespace WebUI.Controllers
     public class AdminAboutController : Controller
     {
         // GET: AdminAbout
+        AboutManager am = new AboutManager(new EfAboutDal());
         public ActionResult Index()
         {
+            var aboutValue = am.GetAboutList();
+            return View(aboutValue);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About about)
+        {
+            am.AboutAdd(about);
+            return RedirectToAction("Index");
         }
     }
 }
