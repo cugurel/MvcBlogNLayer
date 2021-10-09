6276e542bc476ad718d34c7a82dd57bb6a839f70
Çağrı Uğurel <cugurel7@gmail.com>
2021-10-10 00:58:43 +0300

    messages

---
+++ WebUI/Controllers/AdminContactController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AdminContactController : Controller
+    {
+        // GET: Contact
+        ContactManager cm = new ContactManager(new EfContactDal());
+        public ActionResult Index()
+        {
+            var contactValues = cm.GetContactList();
+            return View(contactValues);
+        }
+
+        public ActionResult GetContactDetails(int id)
+        {
+            var contactValues = cm.GetById(id);
+            return View(contactValues);
+        }
+    }
+}
